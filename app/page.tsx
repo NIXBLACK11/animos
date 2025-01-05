20664a64a2a9223c816fcd9b3b80e8c403e59953
NIXBLACK11 <siddharthsinghrana11@gmail.com>
2025-01-05 15:13:06 +0530

    setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { TipTap } from "@/components/TipTap";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-screen bg-[#ffffff]">
+      <TipTap />
+    </div>
+  );
+}
