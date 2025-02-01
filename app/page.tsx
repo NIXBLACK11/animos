5a5edd8171e7fb29cec09e153c4b98210b74aa67
NIXBLACK11 <siddharthsinghrana11@gmail.com>
2025-02-01 13:49:19 +0530

    analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Hero } from "@/components/Hero";
+import { Analytics } from "@vercel/analytics/react";
 import { BottomGIF } from "@/components/ui/BottomGIF";
 import { ImageBack } from "@/components/ui/ImageBack";
 
 export default function Home() {
 	return (
 		<div className="h-screen w-screen flex flex-col items-center">
+			<Analytics />
 			<ImageBack />
 			<Hero />
 			<BottomGIF />
